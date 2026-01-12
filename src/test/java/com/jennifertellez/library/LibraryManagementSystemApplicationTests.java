88b8e50075797fc1655f9c5d2760fcf94687c549
Jennifer Tellez Vera <102021487+jennyfertellez@users.noreply.github.com>
2026-01-12 11:43:14 -0800

    Initial setup: entities and repository

---
+++ src/test/java/com/jennifertellez/library/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jennifertellez.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

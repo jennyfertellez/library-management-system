06eed523057e1c19b4ede5365e6d39855c4b0d3b
Jennifer Tellez Vera <102021487+jennyfertellez@users.noreply.github.com>
2026-02-05 23:43:54 -0800

    Adding manga api

---
+++ src/main/java/com/jennifertellez/library/dto/jikan/JikanMangaResponse.java
@@ -0,0 +1,110 @@
+package com.jennifertellez.library.dto.jikan;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JikanMangaResponse {
+
+    @JsonProperty("data")
+    private List<JikanMangaResponse> data;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class JikanMangaData {
+
+        @JsonProperty("mal_id")
+        private Long malId;
+
+        @JsonProperty("title")
+        private String title;
+
+        @JsonProperty("title_english")
+        private String titleEnglish;
+
+        @JsonProperty("title_japanese")
+        private String titleJapanese;
+
+        @JsonProperty("authors")
+        private List<Author> authors;
+
+        @JsonProperty("synopsis")
+        private String synopsis;
+
+        @JsonProperty("volumes")
+        private Integer volumes;
+
+        @JsonProperty("chapters")
+        private Integer chapters;
+
+        @JsonProperty("status")
+        private String status;
+
+        @JsonProperty("published")
+        private String published;
+
+        @JsonProperty("score")
+        private Double score;
+
+        @JsonProperty("images")
+        private Images images;
+
+        @JsonProperty("genres")
+        private List<Genre> genres;
+
+        @JsonProperty("themes")
+        private List<Theme> themes;
+
+        @Data
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class Author {
+            @JsonProperty("name")
+            private String name;
+        }
+
+        @Data
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class Published {
+            @JsonProperty("from")
+            private String from;
+
+            @JsonProperty("to")
+            private String to;
+        }
+
+        @Data
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class Images {
+            @JsonProperty("jpg")
+            private ImageUrls jpg;
+
+            @Data
+            @JsonIgnoreProperties(ignoreUnknown = true)
+            public static class ImageUrls {
+                @JsonProperty("image_url")
+                private String imageUrl;
+
+                @JsonProperty("large_image_url")
+                private String largeImageUrl;
+            }
+        }
+
+        @Data
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class Genre {
+            @JsonProperty("name")
+            private String name;
+        }
+
+        @Data
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class Theme {
+            @JsonProperty("name")
+            private String name;
+        }
+    }
+}
